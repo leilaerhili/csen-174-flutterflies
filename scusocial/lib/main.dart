4ea6007f0e30e9d2f69850d930251f1ae01605db
tyler costello <tyc642@gmail.com>
2025-01-22 16:06:44 -0800

    Added timestamp to comments

--- scusocial/lib/main.dart
+++ scusocial/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Event Social Media App',
+      title: 'Herd',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: SignInPage(),
     );
