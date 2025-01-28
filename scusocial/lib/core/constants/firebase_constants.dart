95f13c514b4c2d3beb89c6e15dfbc846329b67ab
Madeline Follosco <mfollosco@scu.edu>
2025-01-28 11:08:57 -0800

    Logic for friend requests

---
+++ scusocial/lib/core/constants/firebase_constants.dart
@@ -0,0 +1,13 @@
+class FirebaseCollectionNames {
+  static const users = "users";
+  static const uid = "uid";
+  FirebaseCollectionNames._();
+}
+
+class FirebaseFieldNames {
+  static const uid = "uid";
+  static const receivedRequests = "receivedRequests";
+  static const sentRequests = "sentRequests";
+  static const friends = "friends";
+  FirebaseFieldNames._();
+}
