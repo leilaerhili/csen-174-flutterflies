6c4defabdcd239b8182239f0924c3cd1a3e44dde
Madeline Follosco <mfollosco@scu.edu>
2025-02-11 10:12:56 -0800

    user class unit testing (friend request methods)

--- scusocial/lib/services/firestore_service.dart
+++ scusocial/lib/services/firestore_service.dart
@@ -26,4 +26,25 @@ class FirestoreService {
       'accepted': [],
     });
   }
+    Future<void> sendFriendRequest(String senderId, String receiverId) async {
+    final senderRef = _firestore.collection('users').doc(senderId);
+    final receiverRef = _firestore.collection('users').doc(receiverId);
+
+    await _firestore.runTransaction((transaction) async {
+      final senderSnapshot = await transaction.get(senderRef);
+      final receiverSnapshot = await transaction.get(receiverRef);
+
+      if (!senderSnapshot.exists || !receiverSnapshot.exists) {
+        throw Exception('User not found');
+      }
+
+      transaction.update(senderRef, {
+        'sentRequests': FieldValue.arrayUnion([receiverId]),
+      });
+
+      transaction.update(receiverRef, {
+        'receivedRequests': FieldValue.arrayUnion([senderId]),
+      });
+    });
+  }
 }
